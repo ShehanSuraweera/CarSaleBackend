4f3f2f2206d949f9457b6a011b9d4d6fa9fe3076
ShehanSuraweera <shehanssuraweera@gmail.com>
2025-01-28 19:56:08 +0530

    remove sessions

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const session = require("express-session");
+// const session = require("express-session");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const adRoutes = require("./routes/adRoutes");
@@ -14,6 +14,7 @@ app.use(
       "http://localhost:3000",
       "https://car-sale-pzbrsc9w8-shehan-suraweeras-projects.vercel.app",
       "https://car-sale-dev.vercel.app",
+      "http://192.168.8.133:3000",
     ],
     credentials: true,
   })
@@ -21,15 +22,15 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  session({
-    key: "userId",
-    secret: "your-secret-key",
-    resave: false,
-    saveUninitialized: false,
-    cookie: { expires: 24 * 60 * 60 * 1000 },
-  })
-);
+// app.use(
+//   session({
+//     key: "userId",
+//     secret: "your-secret-key",
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: { expires: 24 * 60 * 60 * 1000 },
+//   })
+// );
 
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
