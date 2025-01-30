b493d2775b127c0761de82036157e6a76b67d357
ShehanSuraweera <shehanssuraweera@gmail.com>
2025-01-31 02:51:07 +0530

    add new function update profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -24,4 +24,33 @@ const profile = async (req, res) => {
   }
 };
 
-module.exports = { profile };
+const updateProfile = async (req, res) => {
+  const { user_id, name, phone, city, avatar_url } = req.body;
+
+  if (!user_id) {
+    return res.status(400).json({ error: "User ID required" });
+  }
+
+  try {
+    const { error } = await supabase
+      .from("profiles")
+      .update({
+        name: name,
+        phone: phone,
+        city: city,
+      })
+      .eq("id", user_id);
+
+    if (error) {
+      return res
+        .status(500)
+        .json({ error: "Database error", message: error.message });
+    }
+
+    res.status(200).json({ message: "Profile updated successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Server error", message: err.message });
+  }
+};
+
+module.exports = { profile, updateProfile };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { profile } = require("../controllers/userController");
+const { profile, updateProfile } = require("../controllers/userController");
 const { validateToken } = require("../middleware/authMiddleware");
+const upload = require("../middleware/multer");
 const router = express.Router();
 
 router.post("/profile", profile);
+router.post("/update", upload.none(), updateProfile);
 
 module.exports = router;
